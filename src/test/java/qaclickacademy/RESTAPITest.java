f94f884f4b6b447e9d73be26fb9da9f1c3b9d293
Atish Dilip Sonawane <atishsonawane11121999@gmail.com>
2024-02-05 17:13:21 +0530

    commit a jenkins file

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -10,14 +10,7 @@ public class RESTAPITest {
 		System.out.println("postJira");
 		System.out.println("postJira2");
 		System.out.println("postJira3");
-		//feere
-		//ffgf
-		//fgf
-		System.out.println("postJira4");
-		System.out.println("postJira5");
-		System.out.println("postJira6");
-		System.out.println("postJira7");
-		
+	
 		
 		
 		
